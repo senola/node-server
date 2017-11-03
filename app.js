8709b0e410c890979241f0b6688f2c362180177f
zhijun_wang <zhijun_wang@kingdee.com>
2017-11-03 18:22:39 +0800

    add sqlit3

--- app.js
+++ app.js
@@ -25,7 +25,24 @@ app.use(requestLog);
 const staticDir = path.join(__dirname, 'static');
 app.use('/static', express.static(staticDir));
 
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database(':memory:');
 
+db.serialize(()=> {
+    db.run('CREATE TABLE lorem (info TEXT)');
+
+    let stmt = db.prepare('INSERT INTO lorem VALUES (?)');
+    for (let i = 0; i < 10; i++) {
+        stmt.run('Ipsum ' + i);
+    }
+    stmt.finalize();
+
+    db.each('SELECT rowid AS id, info FROM lorem', (err, row)=> {
+        logger.info(row.id + ': ' + row.info);
+    });
+});
+
+db.close();
 app.set('trust proxy', 1); // trust first proxy
 app.use(session({
     secret: config.cookieSecrect,
