443e65a95175ab964380debae8a29fc213ea680d
zhijun_wang <zhijun_wang@sui.com>
2018-01-11 13:19:37 +0800

    add code

--- core/server/utils/proxyCheck.js
+++ core/server/utils/proxyCheck.js
@@ -5,8 +5,27 @@
  */
 const axios = require('axios');
 const api = 'http://cnodejs.org/api/v1/topics'; // 使用cnodejs社区API做测试
+const Model = require('../models');
 
-const proxyCheck = async function(options) {
+const checkExistIp = async function () {
+    const proxyIps = await Model.ProxyIp.findAll();
+
+    for(let i = 0; i < proxyIps.length; i++) {
+        if(! await proxyCheck(proxyIps[i])) {
+            // 删除无效代理
+            const deleteItem = await Model.ProxyIp.destroy({
+                where: {
+                    id: proxyIps[i].id
+                }
+            });
+            const result = '【' + proxyIps[i].serverAddress + ' '+ proxyIps[i].ipAddress + ":" + proxyIps[i].port + '】已清理';
+            console.log(deleteItem === 1 ? result : '');
+        }
+    }
+    console.log('\n 清理完成, 一共有【' +  await Model.ProxyIp.count() + '】 个有效IP');
+};
+
+const proxyCheck = async function(options, checkExistIp) {
     try {
         const res = await axios.request({
             url: api,
@@ -18,6 +37,7 @@ const proxyCheck = async function(options) {
                 port: options.port
             }
         });
+        console.log('【' + options.serverAddress + ' '+ options.ipAddress + ":" + options.port + '】代理有效。');
         return res.status >= 200 && res.status < 400;
     } catch (err) {
         console.log('【' + options.serverAddress + ' '+ options.ipAddress + ":" + options.port + '】代理无效。' + err);
@@ -25,4 +45,7 @@ const proxyCheck = async function(options) {
     }
 };
 
-module.exports = proxyCheck;
+module.exports = {
+    proxyCheck,
+    checkExistIp
+};
